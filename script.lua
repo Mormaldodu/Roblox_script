20f9b84d74bc1d29485d1f1db79cac76e6f68723
Mormaldodu <adamarsyad.khairul@gmail.com>
2026-01-10 20:32:37 +0800

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,7 @@
+print("GitHub script loaded successfully!")
+
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Success",
+    Text = "Your GitHub Lua script is working 🎉",
+    Duration = 5
+})
